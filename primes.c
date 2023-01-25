b30baf82a2e02b03ee54ed51d34d1f1bae4dd22e
sosoariar <sosoariar@163.com>
2023-01-25 15:53:49 +0800

    add user programe prime.c -- init comments

---
+++ primes.c
@@ -0,0 +1,9 @@
+/*
+ * Created by soso on 2023-01-25.
+ *
+ * Write a concurrent version of prime sieve using pipes. This idea is due to Doug McIlroy, inventor of Unix pipes. The picture halfway down this page and the surrounding text explain how to do it. Your solution should be in the file user/primes.c.
+ *
+ * 使用管道编写一个prime sieve的并发版本,
+ * */
+
+
