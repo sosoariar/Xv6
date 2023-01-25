9379eff8e7f59dc9c553b5daa80fbe33783fd8e4
sosoariar <sosoariar@163.com>
2023-01-25 16:45:35 +0800

    add user programe find.c --init programe

---
+++ find.c
@@ -0,0 +1,6 @@
+/*
+ * Write a simple version of the UNIX find program: find all the files in a directory tree with a specific name. Your solution should be in the file user/find.c.
+ *
+ * 指定目录名的所有文件
+ *
+*/
