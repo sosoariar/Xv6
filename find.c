c5a9655ecdee6603d1b96ab69ae92dc7b63ab8d3
sosoariar <sosoariar@163.com>
2023-01-25 17:01:26 +0800

    add user programe find.c --step 01

--- find.c
+++ find.c
@@ -4,3 +4,23 @@
  * 指定目录名的所有文件
  *
 */
+
+#include "kernel/types.h"
+#include "kernel/fcntl.h"
+#include "kernel/stat.h"
+#include "kernel/fs.h"
+#include "user/user.h"
+
+
+int main(int argc, char *argv[]){
+
+    if(argc<3){
+        printf("find: find <path> <fileName>\n");
+        exit(0);
+    }
+
+    find(argv[1], argv[2]);
+    exit(0);
+
+}
+
